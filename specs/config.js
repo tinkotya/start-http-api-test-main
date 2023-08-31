fc2512f1a0159f9af408fb8d8aa44b489b2b1d2f
tinkotya <kapibara97@yandex.ru>
2023-08-31 12:19:31 +0300

    апи тесты

---
+++ specs/config.js
@@ -0,0 +1,10 @@
+const config = {
+    url: 'https://bookstore.demoqa.com',
+    credentials: {
+        "userName": "tinkotya",
+        "password": "Tinkotya@19071997"
+    },
+    uuid: '4d949cab-d771-4a44-ba88-7c3e54d4b91a'
+}
+
+export default config;
